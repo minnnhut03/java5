445b2a8d1b164dd3b072a1b40c8379842da95739
Nghindpc <nghindpc08445@gmail.com>
2025-02-17 17:51:15 +0700

    uessr\\\\

--- src/main/java/com/java5/demoJV5/entity/UserEntity.java
+++ src/main/java/com/java5/demoJV5/entity/UserEntity.java
@@ -1,5 +1,10 @@
 package com.java5.demoJV5.entity;
 
 public class UserEntity {
-
+	private int id;
+	private String username;
+	private String password;
+	private String name;
+	private String email;
+	
 }
